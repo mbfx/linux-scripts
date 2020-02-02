90846743050e29f25a24d805dc2591d40271a104
Aleksandr Gavrik <48349530+mbfx@users.noreply.github.com>
2020-02-02 20:56:20 +0200

    some updates

--- otus-workstation/otus-ws-base.sh
+++ otus-workstation/otus-ws-base.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/bash
 
-echo "Installing wget and vim..."
+echo "Installing EPEL repository..."
+yes | yum -y -q install epel-release
+
+echo "Installing wget/vim/git/python3/ansible..."
 yes | yum -y -q install wget vim
+yes | yum -y -q install git
+yes | yum -y -q install python3
+yes | yum -y -q install ansible
 
 echo "Installing VirtualBox repo..."
 if [[ ! -e /etc/yum.repos.d/virtualbox.repo ]]
@@ -11,19 +17,13 @@ if [[ ! -e /etc/yum.repos.d/virtualbox.repo ]]
         "File /etc/yum.repos.d/virtualbox.repo exist!"
 fi 
 
-echo "Installing EPEL repository..."
-yes | yum -y -q install epel-release
-
-echo "Installing VirtualBox 5.2 and prerequisites..."
+echo "Installing VirtualBox 6.1 and prerequisites..."
 yes | yum -y -q install dkms kernel-devel
 yes | yum -y -q install groupinstall "Development Tools"
-yes | yum -y -q install VirtualBox-5.2
+yes | yum -y -q install VirtualBox-6.1
 usermod -a -G vboxusers mbfx
 
-echo "Installing Vagrant 2.2.5..."
-yes | yum -y -q install https://releases.hashicorp.com/vagrant/2.2.5/vagrant_2.2.5_x86_64.rpm
-
-echo "Installing Git..."
-yes | yum -y -q install git
+echo "Installing Vagrant 2.2.7..."
+yes | yum -y -q install https://releases.hashicorp.com/vagrant/2.2.7/vagrant_2.2.7_x86_64.rpm
 
 exit 0
